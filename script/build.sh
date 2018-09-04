43a24eb0d3efeebddca75ee9c0abf671ebe00714
Perki <pm@perki.com>
2018-09-04 15:46:34 +0200

    added build logic

---
+++ script/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cd ..
+cp -r src/*.html gh-pages/
---
+++ script/setup-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cd ..
+git clone git@github.com:perki/pryv-web-demoflow.git --branch gh-pages ./gh-pages
