d8a277c73a65caf5f241f369e6666c33fb6ec952
Perki <pm@perki.com>
2018-10-10 11:51:25 +0200

    cleanup

--- script/access-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-cd ../app-web-campaign-manager
-node build/build.js
-cp -r dist/* ../gh-pages/cm
-
-
--- script/setup-dev.sh
+++ script/setup-dev.sh
@@ -4,8 +4,3 @@ cd ..
 if [[ ! -d ./gh-pages ]]; then
     git clone git@github.com:perki/pryv-web-demoflow.git --branch gh-pages --single-branch ./gh-pages
 fi
-if [[ ! -d ./app-web-campaign-manager ]]; then
-    git clone git@github.com:pryv/app-web-campaign-manager.git --branch app-web-demoflow --single-branch ./app-web-campaign-manager
-    cd app-web-campaign-manager
-    yarn install
-fi
