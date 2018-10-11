d015c37eaedfc5d389e68276c145ba88f8228d97
Perki <pm@perki.com>
2018-10-11 18:06:09 +0200

    added details

--- src/audit/src/components/Accesses.vue
+++ src/audit/src/components/Accesses.vue
@@ -22,11 +22,25 @@
       </tr>
     </template>
     <template slot="items" slot-scope="props">
-      <tr :active="props.selected" @click="props.selected = !props.selected">
+      <tr :active="props.selected" @click="props.expanded = !props.expanded">
         <td v-for="field in fields" class="text-xs-left">
           {{ field.display(props.item) }}
         </td>
       </tr>
+    </template>
+    <template slot="expand" slot-scope="props">
+      <v-card flat>
+        <v-list two-line>
+          <template v-for="detail in details">
+            <v-list-tile >
+              <v-list-tile-content>
+                <span class='text--primary'>{{ detail.title }}</span>&mdash; {{ detail.display(props.item)}}
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+        <v-card-text></v-card-text>
+      </v-card>
     </template>
   </v-data-table>
 
@@ -81,6 +95,16 @@
     return '-';
   }
 
+  function displayDeleted(access) {
+    if (access.deleted === null) return 'Expired';
+    return 'Valid';
+  }
+
+  function displayPermission(access) {
+    if (access.type === 'personal') return 'System or Owner';
+    return access.permissions;
+  }
+
   const fields = [
     {
       key: 'name',
@@ -98,6 +122,11 @@
       display: displayDate('lastUsed')
     },
     {
+      key: 'deleted',
+      title: 'Status',
+      display: displayDeleted
+    },
+    {
       key: 'created',
       title: 'Creation Date',
       display: displayDate('created')
@@ -106,18 +135,26 @@
       key: 'createdBy',
       title: 'Created By',
       display: displayBy('createdBy')
-    },
+    }
+    ];
+
+  const details = [
     {
       key: 'modified',
       title: 'Modification Date',
-      display: modifiedDate
+      display: modifiedDate,
     },
     {
       key: 'modifiedBy',
       title: 'Modified By',
-      display: modifiedBy
+      display: modifiedBy,
+    },
+    {
+      key: 'permission',
+      title: 'Permissions',
+      display: displayPermission,
     }
-  ]
+  ];
 
 
 
@@ -129,6 +166,7 @@
         UserInput :'',
         accesses: this.accesses,
         fields: fields,
+        details: details,
         pagination: {
           sortBy: 'name',
           rowsPerPage: 15
