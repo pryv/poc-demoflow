88749d61bf20cee504fdd20e2d200455d5dc27cf
Perki <pm@perki.com>
2018-10-11 16:46:42 +0200

    displaying all accesses

--- src/audit/src/components/Accesses.vue
+++ src/audit/src/components/Accesses.vue
@@ -1,15 +1,14 @@
 <template>
   <table border="1">
     <tr>
-      <th>name</th><th>username</th>
-    </tr>
+      <th v-for="field in fields">
+        {{ field.title }}
+      </th>
+    <tr>
 
     <tr v-for="access in accesses">
-      <td>
-        {{ access.name }}
-      </td>
-      <td>
-        {{ access.token }}
+      <td v-for="field in fields">
+        {{ field.display(access) }}
       </td>
     </tr>
   </table>
@@ -18,7 +17,89 @@
 
 <script>
 
+  var accessesMap = {};
 
+
+  function displayType (access) {
+    if (access.type === 'shared') return 'Sharing';
+    if (access.type === 'app') {
+      if (access.name.startsWith('cm-')) return 'Consent';
+      return 'Device & App';
+    }
+    if (access.type == 'personal') return 'Management tools';
+    return 'Unknown';
+  }
+
+  function displayRaw (key) {
+    return function (access) {
+      return access[key];
+    }
+  }
+
+  function displayDate (key) {
+    return function (access) {
+      var date = new Date(access[key] * 1000);
+      return date.toLocaleString();
+    }
+  }
+
+  function displayBy (key) {
+    return function (access) {
+      if (access[key] === 'system') return 'System or Owner';
+      if (accessesMap[access[key]]) {
+        return accessesMap[access[key]].name;
+      }
+      return accessesMap[access[key]];
+    }
+  }
+
+  function modifiedDate (access) {
+    if (access.modified !== access.created) return displayDate('modified')(access);
+    return '-';
+  }
+
+  function modifiedBy(access) {
+    if (access.modifiedBy !== access.createdBy) return displayBy('modified')(access);
+    return '-';
+  }
+
+  const fields = [
+    {
+      key: 'name',
+      title: 'Name',
+      display: displayRaw('name')
+    },
+    {
+      key: 'type',
+      title: 'Type',
+      display: displayType
+    },
+    {
+      key: 'lastUsed',
+      title: 'Last Usage',
+      display: displayDate('lastUsed')
+    },
+    {
+      key: 'created',
+      title: 'Creation Date',
+      display: displayDate('created')
+    },
+    {
+      key: 'createdBy',
+      title: 'Created By',
+      display: displayBy('createdBy')
+    },
+    {
+      key: 'modified',
+      title: 'Modification Date',
+      display: modifiedDate
+    },
+    {
+      key: 'modifiedBy',
+      title: 'Modified By',
+      display: modifiedBy
+    }
+  ]
 
 
   export default {
@@ -27,7 +108,8 @@
       return {
         username: window.pryvUsername,
         UserInput :'',
-        accesses: this.accesses
+        accesses: this.accesses,
+        fields: fields
       }
 
     },
@@ -42,6 +124,9 @@
           console.log(JSON.stringify(res));
           if (res) {
             that.accesses = res;
+            that.accesses.forEach(function (access) {
+              accessesMap[access.id] = access;
+            });
           }
       });
     },
@@ -51,4 +136,7 @@
       }
     }
   }
+
+
+
 </script>
