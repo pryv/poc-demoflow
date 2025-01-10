00d64d1de1772a90ed57891d11cacb291455b03a
Perki <perki@pryv.com>
2025-01-10 13:47:01 +0100

    changing setup

--- script/setup-dev.sh
+++ script/setup-dev.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 cd ..
 if [[ ! -d ./gh-pages ]]; then
-    git clone git@github.com:perki/pryv-web-demoflow.git --branch gh-pages --single-branch ./gh-pages
+    git clone git@github.com:pryv/poc-demoflow.git --branch gh-pages --single-branch ./gh-pages
 fi
